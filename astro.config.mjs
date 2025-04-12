eef510c0b69c94b016aa1891a2e4241f21f45270
grishaab <gbandodkar@ucdavis.edu>
2025-04-12 18:55:55 -0400

    modify url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://behav-ai.com',
-  base: 'landing',
-})
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
