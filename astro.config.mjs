e1e9f916066e70abc845df6c1be595c40e4cb965
grishaab <gbandodkar@ucdavis.edu>
2025-04-12 18:48:48 -0400

    deploy command

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+  // site: 'https://behav-ai.com',
+  base: 'landing',
+})
